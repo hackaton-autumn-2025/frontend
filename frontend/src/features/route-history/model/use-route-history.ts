70fd8805f20712ddac919f9014f298a0b6d6740f
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-10-26 05:30:43 +0300

    feat: работы с историей простроенных маршрутов

---
+++ frontend/src/features/route-history/model/use-route-history.ts
@@ -0,0 +1,11 @@
+import useRouteHistory from "@/entities/route/model/use-route-history";
+
+const useFetchRouteHistory = () => {
+    const { data: routeHistory } = useRouteHistory();
+
+    return {
+        routeHistory,
+    }
+}
+
+export default useFetchRouteHistory;
