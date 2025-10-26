a15a7a52648fe98564352c76e32e052a7ff5a1b4
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-10-26 05:33:57 +0300

    config/ui: основные layout для авторизации и основной части приложения

---
+++ frontend/src/pages/AuthLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {useUserAuthenticated} from "@/features/auth/model/use-user-authenticated";
+useUserAuthenticated()
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/pages/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from "@/widgets/header/header.vue";
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
