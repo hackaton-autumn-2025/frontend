36c135f0d1b517df8f427a6f90ed1a505568817c
Nikita Zabashtin <nekitov.maksim@gmail.com>
2025-10-26 05:24:35 +0300

    core: основной файл App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppSnackbar from "@/shared/ui/app-snackbar.vue";
+</script>
+
+<template>
+  <v-container>
+    <RouterView />
+    <AppSnackbar />
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
